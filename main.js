73af2fa6e9bc9d446f2a16cb1d55afde313edfd6
akashyadav2311 <rajuyarr80@gmail.com>
2024-12-05 07:08:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import { start } from './script.js';
+
+// This can be done because start is now exported
+document.querySelector('button').addEventListener('onclick', start());
---
+++ script.js
@@ -0,0 +1,6 @@
+import quizData from "../data";
+
+export function start() {
+    console.log("hello World");
+    console.log(quizData);
+}
